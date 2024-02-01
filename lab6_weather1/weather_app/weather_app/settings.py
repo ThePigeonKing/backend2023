6265d675dbb302bf12316026703138a47b935dbd
ThePigeonKing <thepigeonking@mail.ru>
2024-02-01 19:28:00 +0300

    weather1: styles started adding

--- lab6_weather1/weather_app/weather_app/settings.py
+++ lab6_weather1/weather_app/weather_app/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # custom settings
 LOGIN_REDIRECT_URL = 'main_page'
 LOGOUT_REDIRECT_URL = 'main_page'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
